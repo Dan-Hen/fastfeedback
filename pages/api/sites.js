bb984eab9f40b8781cbd8b285679be4567e57f58
Dan-Hen <leo.clot16@gmail.com>
2021-03-31 19:52:24 +0200

    show site accessed by user

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,9 +1,20 @@
 import db from '../../lib/firebase-admin';
 
 export default async (req, res) => {
-  const snapshot = await db.collection('sites').get();
 
   const sites = [];
+  //obtenir id utilisateurs
+  console.log(typeof req.cookies.user)
+  const user = req.cookies.user;
+  const parsedUser = JSON.parse(user);
+  const userId = parsedUser.id;
+
+  //récuperer les sites
+  const snapshot = await db.collection('sites')
+    .where('ownerId', '==', userId)
+    .get();
+
+  //afficher les sites que l'utilisateur à accès
 
   snapshot.forEach((doc) => {
     sites.push({ id: doc.id, ...doc.data() });
