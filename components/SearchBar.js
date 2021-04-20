03104e02acb658e79c87aed60f43ed0ed28798d2
Dan-Hen <leo.clot16@gmail.com>
2021-04-20 19:20:07 +0200

    search bar code

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 import {
   Box,
   Input,
   IconButton
 } from '@chakra-ui/react'
-import { SearchIcon } from '@chakra-ui/icons'
+import { SearchIcon, TimeIcon } from '@chakra-ui/icons'
 
-const SearchBar = () => (
-  <Box display="flex" justifyContent="flex-end" mb={'20px'}>
-    <Input
-      width="40%"
-    />
-    <IconButton aria-label="icon" icon={<SearchIcon />} size="md" />
-  </Box>
-)
+const SearchBar = () => {
+  const [search, setSearch] = useState('');
+
+  return (
+    <Box display="flex" justifyContent="flex-end" mb={'20px'}>
+      <Input
+        width="40%"
+        value={search}
+        onChange={(event) => {
+          setSearch(event.target.value);
+          console.log(event.target.value)
+        }}
+      />
+      <IconButton
+        aria-label="icon"
+        icon={<SearchIcon />}
+        size="md"
+      />
+      <IconButton
+        aria-label="icon"
+        icon={<TimeIcon />}
+        size="md"
+        onClick={() => setSearch('')}
+      />
+    </Box>
+  );
+};
 
 export default SearchBar
